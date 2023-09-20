5cfde0d1ad59ee2a8bf5df0c78ced09eae2c7a7c
Diar Begisbayev <diar_beg64@mail.ru>
2023-09-21 03:05:20 +0600

    Created posts UI, changed main color

--- frontend/src/app/teams/submodules/posts/posts.module.ts
+++ frontend/src/app/teams/submodules/posts/posts.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsComponent } from './pages/posts/posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { PostsRoutingModule } from './posts-routing.module';
+import { PostItemComponent } from './components/post-item/post-item.component';
 
 @NgModule({
-  declarations: [PostsComponent],
-  imports: [CommonModule, SharedModule],
+  declarations: [PostsComponent, PostItemComponent],
+  imports: [CommonModule, SharedModule, PostsRoutingModule],
 })
 export class PostsModule {}
