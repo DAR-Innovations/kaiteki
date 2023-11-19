5487e78f45e3c394d7cfd996124b2c8699fa3c09
Diar Begisbayev <diar_beg64@mail.ru>
2023-11-19 16:44:28 +0600

    Submodules

--- frontend/src/app/teams/submodules/tasks/pages/task-view/task-view.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-task-view-page',
-  templateUrl: './task-view.component.html',
-  styleUrls: ['./task-view.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class TaskViewComponent {}
